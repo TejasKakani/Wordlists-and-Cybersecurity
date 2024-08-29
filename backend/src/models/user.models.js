9487949ef62ff1c82473e7e9b73af5c95bb167da
KAKANI TEJAS RAJESH <tejaskakani.official@gmail.com>
2024-08-29 20:14:28 +0530

    added user model

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,64 @@
+const moongose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+/*
+    username
+    email
+    password
+    refreshToken
+    createdAt
+    updatedAt
+*/
+
+const userSchema = new moongose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+        index: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+},
+{
+    timestamps: true // createdAt, updatedAt
+});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id
+    }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRATION});
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_TOKEN_EXPIRATION});
+}
+
+module.exports = moongose.model('User', userSchema);
